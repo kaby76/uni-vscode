a06a2fb9683d48982ed7e7ff4395e8a6ae4eae91
Jort Rodenburg <Jort.Rodenburg@acronis.com>
2024-02-16 13:47:47 -0600

    Add brew installation of vsce on Darwin

--- VsCode/install.sh
+++ VsCode/install.sh
@@ -12,6 +12,11 @@ npm i vscode-languageserver@7.0.0-next.7
 npm i vscode-languageserver-protocol@3.16.0-next.7
 npm i vscode-languageserver-types@3.16.0-next.3
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install vsce
+fi
+
+
 cp -r ../Server/bin/Debug/net7.0 ./server
 npm install
 npm run compile
